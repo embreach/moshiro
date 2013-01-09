f7e8750d5cb672c0001cc00e5728b3a8dd39318b
Jari Aarniala <jari.aarniala@gmail.com>
2013-01-09 20:59:57 +0200

    Use RequireJS

---
+++ app/main.js
@@ -0,0 +1,9 @@
+require({
+  paths: {
+    'cs': '../lib/cs',
+    'coffee-script': '../lib/coffee-script',
+    'jquery': '../lib/jquery',
+    'bacon': '../lib/Bacon.min'
+  },
+  baseUrl: 'app'
+}, ["cs!moshiro"])
